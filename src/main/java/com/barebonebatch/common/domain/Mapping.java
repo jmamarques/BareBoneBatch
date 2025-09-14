a3a94058b55d95556ee54e9ac334c17fea96f75b
João Marques <24671080+jmamarques@users.noreply.github.com>
2025-09-14 23:09:15 +0100

    add dependencies

--- src/main/java/com/barebonebatch/common/domain/Mapping.java
+++ src/main/java/com/barebonebatch/common/domain/Mapping.java
@@ -3,6 +3,8 @@ package com.barebonebatch.common.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class Mapping {
@@ -10,4 +12,5 @@ public class Mapping {
     private Integer iden;
     private String id;
     private String mappingType;
+    private List<MappingFields> fields;
 }
--- src/main/java/com/barebonebatch/common/domain/WorkStatus.java
+++ src/main/java/com/barebonebatch/common/domain/WorkStatus.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -24,5 +25,7 @@ public class WorkStatus {
     private LocalDateTime endDate;
 
     private String errorText;
+
+    private List<ImportLine> importLines;
 
 }
